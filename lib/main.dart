7537febe5dc0db5a107e51b2c16e46bb5313168e
Everton Barrett <ebarrton1@gmail.com>
2026-07-07 12:39:39 -0400

    Wrap ChurchSnap app with ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
 import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const ChurchSnapApp());
+  runApp(const ProviderScope(child: ChurchSnapApp()));
 }
