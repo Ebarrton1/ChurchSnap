dfa83b7d81a512574af84cf8e069f8969ddf9f19
Everton Barrett <ebarrton1@gmail.com>
2026-07-07 12:34:03 -0400

    Add Riverpod repository providers

---
+++ lib/core/providers/repository_providers.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../features/announcements/repositories/announcement_repository.dart';
+import '../../features/events/repositories/event_repository.dart';
+
+final announcementRepositoryProvider = Provider<AnnouncementRepository>((ref) {
+  return AnnouncementRepository();
+});
+
+final eventRepositoryProvider = Provider<EventRepository>((ref) {
+  return EventRepository();
+});
