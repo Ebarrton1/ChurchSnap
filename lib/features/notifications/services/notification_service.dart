8ce90c8c484e69102af51ccc3f7441f703e611ae
Everton Barrett <ebarrton1@gmail.com>
2026-07-08 00:09:03 -0400

    Save FCM token to member profile

--- lib/features/notifications/services/notification_service.dart
+++ lib/features/notifications/services/notification_service.dart
@@ -1,5 +1,6 @@
 import 'dart:developer' as developer;
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
@@ -9,26 +10,15 @@ class NotificationService {
   static final NotificationService instance = NotificationService._();
 
   final FirebaseMessaging _messaging = FirebaseMessaging.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<void> initialize() async {
     await _messaging.requestPermission();
 
     final token = await _messaging.getToken();
-    final userId = FirebaseAuth.instance.currentUser?.uid;
+    await _saveToken(token);
 
-    if (token != null) {
-      developer.log(
-        'FCM token received: $token',
-        name: 'ChurchSnap.Notifications',
-      );
-    }
-
-    if (userId != null) {
-      developer.log(
-        'Signed-in user ready for FCM token save: $userId',
-        name: 'ChurchSnap.Notifications',
-      );
-    }
+    FirebaseMessaging.instance.onTokenRefresh.listen(_saveToken);
 
     FirebaseMessaging.onMessage.listen((message) {
       developer.log(
@@ -40,5 +30,34 @@ class NotificationService {
     FirebaseMessaging.onMessageOpenedApp.listen((message) {
       developer.log('Notification tapped', name: 'ChurchSnap.Notifications');
     });
+  }
+
+  Future<void> _saveToken(String? token) async {
+    if (token == null) return;
+
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) {
+      developer.log(
+        'No signed-in user available for FCM token save.',
+        name: 'ChurchSnap.Notifications',
+      );
+      return;
+    }
+
+    await _firestore
+        .collection('churches')
+        .doc('demo-church')
+        .collection('members')
+        .doc(user.uid)
+        .set({
+          'fcmToken': token,
+          'fcmTokenUpdatedAt': FieldValue.serverTimestamp(),
+        }, SetOptions(merge: true));
+
+    developer.log(
+      'FCM token saved for user ${user.uid}',
+      name: 'ChurchSnap.Notifications',
+    );
   }
 }
